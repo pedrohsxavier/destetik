9c6388ec13f6d499a696b22f6d3615a618a6e69c
unknown <pah.dsf@gmail.com>
2019-04-09 17:44:10 -0300

    Fixing fields of register

--- app/user/model.js
+++ app/user/model.js
@@ -2,48 +2,38 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    phone:{
-      type: String,
-      required: true
-    },
-    email:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    address: {
-      street: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true
+  },
+  phone: String,
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  address: {
+    street: String,
+    district: String,
+    houseNumber: String,
+    geoLocation: {
+      lat: {
+        type: Number
       },
-      district: {
-        type: String,
-        required: true
-      },
-      houseNumber: {
-        type: Number,
-        required: true
-      },
-      geoLocation: {
-        lat: {
-          type: Number
-        },
-        lng: {
-          type: Number
-        }
+      lng: {
+        type: Number
       }
-    },
-    services: [{
+    }
+  },
+  services: [
+    {
       type: Schema.Types.ObjectId,
       ref: 'service'
-    }]
-})
+    }
+  ]
+});
 
 export default mongoose.model('user', UserSchema);
