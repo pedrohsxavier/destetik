53d9d609cce3c427f38b34910c4aae2380ce3e27
Willian Abdon <willian.abdom@hotmail.com>
2019-04-13 03:20:56 -0300

    desenvolvimento da funcionalidade de atualização de usuário

--- app/user/controller.js
+++ app/user/controller.js
@@ -24,6 +24,32 @@ class UserController {
     } catch (err) {
       console.log(err);
     }
+  }  
+
+  async update(req, res, next){
+    try {
+      if ("old_password" in req.body) {
+        var user = await User.findById(req.params.id);
+        const { old_password, password, confirm_password } = req.body;
+        const pass_ok = await bcrypt.compare(old_password, user.password);
+        if (!pass_ok)
+        return res.json({ error: 'Wrong Password.' });
+        if (password !== confirm_password)
+          return res.json({ error: 'Password does not Match.' });    
+          
+        delete req.body["old_password"]
+        delete req.body["confirm_password"]
+        req.body["password"] = await bcrypt.hash(password, 10);
+      }
+      user = await User.findOneAndUpdate(
+        {"_id" : req.params.id}, 
+        { $set: req.body }, 
+        { useFindAndModify: false, new : true }
+      );
+      return res.json(user);      
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
--- app/user/router.js
+++ app/user/router.js
@@ -3,5 +3,6 @@ import UserController from './controller';
 const userRouter = express.Router();
 
 userRouter.post('/register', UserController.store);
+userRouter.patch('/update/:id', UserController.update);
 
 export default userRouter;
