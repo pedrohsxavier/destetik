3add8cb3dcb862a09c685ea138851831abc97889
Pah Ferreira <pah.dsf@gmail.com>
2019-04-15 19:52:56 -0300

    installing Reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBarDestetik from './layout/NavBarDestetik';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <NavBarDestetik />
         Success!
       </div>
     );
